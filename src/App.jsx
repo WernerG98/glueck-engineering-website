92412231cc409ec31ee75adc2fd7c6f49b5a55be
Werner Glück <werner.glueck@gmx.net>
2026-09-10 18:31:36 +0200

    Scroll to top on page navigation
    React Router doesn't reset scroll position on route changes, so
    navigating between pages left the viewport wherever the previous page
    was scrolled to. Added a ScrollToTop component that resets scroll on
    every pathname change, using an instant jump rather than the site's
    global smooth-scroll so it doesn't animate during navigation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 import HomePage from "./pages/HomePage";
 import FertigteilePage from "./pages/FertigteilePage";
 import ArtworksPage from "./pages/ArtworksPage";
@@ -12,17 +13,20 @@ import NotFoundPage from "./pages/NotFoundPage";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/service" element={<ServicePage />} />
-      <Route path="/fertigteile" element={<FertigteilePage />} />
-      <Route path="/artworks" element={<ArtworksPage />} />
-      <Route path="/materialien" element={<MaterialienPage />} />
-      <Route path="/faq" element={<FAQPage />} />
-      <Route path="/impressum" element={<ImpressumPage />} />
-      <Route path="/datenschutz" element={<DatenschutzPage />} />
-      <Route path="/agb" element={<AGBPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/service" element={<ServicePage />} />
+        <Route path="/fertigteile" element={<FertigteilePage />} />
+        <Route path="/artworks" element={<ArtworksPage />} />
+        <Route path="/materialien" element={<MaterialienPage />} />
+        <Route path="/faq" element={<FAQPage />} />
+        <Route path="/impressum" element={<ImpressumPage />} />
+        <Route path="/datenschutz" element={<DatenschutzPage />} />
+        <Route path="/agb" element={<AGBPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
   );
 }
